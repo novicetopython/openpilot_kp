4d71d4e082d8963185571d1ea4c13f0b43aa3176
novicetopython <35001563+novicetopython@users.noreply.github.com>
2025-09-07 18:53:54 +0100

    Update carstate.py

--- opendbc_repo/opendbc/car/hyundai/carstate.py
+++ opendbc_repo/opendbc/car/hyundai/carstate.py
@@ -1078,8 +1078,8 @@ class CarState(CarStateBase):
     ]
 
 #    if CP.openpilotLongitudinalControl and CP.sccBus == 2 or (CP.flags & HyundaiFlags.CAMERA_SCC):
-     if not CP.openpilotLongitudinalControl and (CP.sccBus == 2 or (CP.flags & HyundaiFlags.CAMERA_SCC)):
-       cam_messages += [
+    if not CP.openpilotLongitudinalControl and (CP.sccBus == 2 or (CP.flags & HyundaiFlags.CAMERA_SCC)):
+      cam_messages += [
         ("SCC11", 50),
         ("SCC12", 50),
       ]
