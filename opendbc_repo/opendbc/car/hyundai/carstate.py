f859c86ebaa28c3b1536d86420b731b5bec122ca
novicetopython <35001563+novicetopython@users.noreply.github.com>
2025-09-08 10:28:32 +0100

    Update carstate.py

--- opendbc_repo/opendbc/car/hyundai/carstate.py
+++ opendbc_repo/opendbc/car/hyundai/carstate.py
@@ -1100,5 +1100,5 @@ class CarState(CarStateBase):
     return {
       Bus.pt: CANParser(DBC[CP.carFingerprint][Bus.pt], pt_messages, 0),
       #Bus.cam: CANParser(DBC[CP.carFingerprint][Bus.pt], cam_messages, 2),
-      Bus.cam: CANParser(DBC[CP.carFingerprint][Bus.cam], cam_messages, 2),  # ✅ use Bus.cam DBC
+      us.cam: CANParser(DBC[CP.carFingerprint].get(Bus.cam, DBC[CP.carFingerprint][Bus.pt]), cam_messages, 2)
     }
